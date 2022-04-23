d973cbf874a2e5659d24ba13b93d2b0262ce6d59
hamza-zaveri <91050243+hamza-zaveri@users.noreply.github.com>
2022-04-23 11:43:05 +0530

    Frontend updated

---
+++ frontend/src/Components/EditProfile.js
@@ -0,0 +1,140 @@
+import React from "react";
+import "../Register.css";
+import { Link } from "react-router-dom";
+
+const EditProfile = () => {
+  return (
+    <>
+      <section class="contact-from pt-4">
+        <div class="container">
+          <div class="row mt-5">
+            <div class="col-md-7 mx-auto">
+              <div class="form-wrapper">
+                <div class="row">
+                  <div class="col-md-12">
+                    <h4>Edit Profile form</h4>
+                  </div>
+                </div>
+                
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <input
+                          type="text"
+                          class="form-control"
+                          placeholder="Full name"
+                        />
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <input
+                          type="text"
+                          class="form-control"
+                          placeholder=" Username"
+                        />
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <input
+                          type="email"
+                          class="form-control"
+                          placeholder="Email"
+                        />
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label>Gender :</label>
+                        <br />
+                        <div class="form-check form-check-inline">
+                          <input
+                            class="form-check-input"
+                            type="radio"
+                            name="inlineRadioOptions"
+                            id="inlineRadio1"
+                            value="option1"
+                          />
+                          <label class="form-check-label" for="inlineRadio1">
+                            Male
+                          </label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                          <input
+                            class="form-check-input"
+                            type="radio"
+                            name="inlineRadioOptions"
+                            id="inlineRadio2"
+                            value="option1"
+                          />
+                          <label class="form-check-label" for="inlineRadio1">
+                            Female
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <input
+                          type="text"
+                          class="form-control"
+                          placeholder="Phone number"
+                        />
+                      </div>
+                    </div>
+
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        Date Of Birth:
+                        <input type="date" class="form-control" />
+                      </div>
+                    </div>
+
+                    <div class="mt-3 mb-3">
+                      <button class="btn btn-primary">Edit</button>
+                    </div>
+
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <input
+                          type="text"
+                          class="form-control"
+                          placeholder="Old Password"
+                        />
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <input
+                          type="text"
+                          class="form-control"
+                          placeholder="New Password"
+                        />
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <input
+                          type="text"
+                          class="form-control"
+                          placeholder="Confirm Password"
+                        />
+                      </div>
+                    </div>
+
+                    <div class="mt-3 mb-3">
+                      <button class="btn btn-primary">Change Password</button>
+                    </div>
+                  </div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+export default EditProfile;
---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,81 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Cookie from "js-cookie";
+const Navbar = () => {
+  const is_logged_in = Cookie.get("token") ? true : false;
+  const navigate = useNavigate();
+  const logout = () => {
+    Cookie.remove("token");
+    window.location.href = "/login";
+  };
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <NavLink className="navbar-brand" to="#">
+          Sleep Tracking System
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse ml-4"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+                  <NavLink className="nav-link" to="/">
+                    Home <span className="sr-only">(current)</span>
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/dashboard">
+                    Dashboard
+                  </NavLink>
+                </li>
+            {!is_logged_in ? (
+              <>
+                
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/login">
+                    <span className="glyphicon glyphicon-log-in"></span> Login
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/register">
+                    <span className="glyphicon glyphicon-user"></span> Register
+                  </NavLink>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/" onClick={logout}>
+                    <span className="glyphicon glyphicon-log-in"></span> Log Out
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/editprofile">
+                    <span className="glyphicon glyphicon-log-in"></span>{" "}
+                    EditProfile
+                  </NavLink>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+export default Navbar;
