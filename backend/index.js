37bf66a0d874a43a8089565bb50ee5acb6772fdb
hamza-zaveri <91050243+hamza-zaveri@users.noreply.github.com>
2022-04-23 11:42:03 +0530

    backend files updated

---
+++ backend/index.js
@@ -0,0 +1,157 @@
+const express=require('express')
+const cors=require("cors");
+require('./conf');
+
+const User=require('./signup.js');
+const sleep_detail=require('./add_sleep_details.js');
+const res = require('express/lib/response');
+
+const app=express();
+
+app.use(express.json())
+app.use(cors());
+// signup
+// app.post("/register",async(req,res)=>{
+//     let data=new user(req.body)
+//     let result=await data.save()
+//     res.send(result)
+// })
+app.post("/register", (req, res)=> {
+    const { name, username, email,phoneNo,dob,password,reEnterPassword} = req.body
+    User.findOne({email: email}, (err, user) => {
+        if(user){
+            res.send({message: "User already registerd"})
+        } else {
+            const user = new User({
+                name,
+                username,
+                email,
+                phoneNo,
+                dob,
+                password,
+                reEnterPassword
+            })
+            user.save(err => {
+                if(err) {
+                    res.send(err)
+                } else {
+                    res.send( { message: "Successfully Registered, Please login now." })
+                }
+            })
+        }
+    })
+    
+})
+
+
+// login
+// app.post("/login",async(req,res)=>{
+//     let loggedin = await user.findOne(req.body).select("-password")
+
+//     if(req.body.username && req.body.password){
+//         if(loggedin){
+//             res.send(loggedin)
+//         }
+//         else{
+//             res.send('not found')
+//         }
+//     }
+//     else{
+//         res.send('enter proper credentials')
+//     }
+   
+// })
+app.post("/login", (req, res)=> {
+    const { username, password} = req.body
+    User.findOne({ username: username}, (err, user) => {
+        if(user){
+            if(password === user.password ) {
+                res.send({message: "Login Successfull", user: user})
+            } else {
+                res.send({ message: "Password didn't match"})
+            }
+        } else {
+            res.send({message: "User not registered"})
+        }
+    })
+})
+
+// add sleep data
+app.post("/add_sleep_details",async(req,res)=>{
+    let sleep_data=new sleep_detail(req.body)
+    let sleep_data_result=await sleep_data.save()
+    res.send(sleep_data_result)
+})
+
+// edit profile
+app.put("/edit_profile/:user",async(req,res)=>{
+    let edit_data=await user.updateOne( 
+        {username: req.params.user},
+        {
+            $set : req.body
+        }
+        )
+
+    res.send(edit_data)
+})
+
+app.listen(5001);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const connectDB=async()=>{
+   
+    
+//     const user=mongoose.model('users',userschema)
+
+//     const data=await user.find();
+
+//     console.warn(data)
+// }
+
+// connectDB();
+
+// app.get("/",(req,res)=>{
+//     res.send('aeeee suruchi it is working............')
+// })
+
---
+++ backend/signup.js
@@ -0,0 +1,24 @@
+const mongoose=require('mongoose');
+
+const userschema=new mongoose.Schema({
+    name : String,
+    username :String,
+    contact_number : Number,
+    email:String,
+    password:String,
+    dob:Date,
+    gender : String
+
+});
+
+module.exports=mongoose.model("users",userschema)
+
+// {
+//     "name":"pranav prajapati",
+//     "username":"pranav._.9",
+//     "contact_number": 9773135682,
+//     "email":"pra9prajapati@gmail.com",
+//     "password":"Pranav@123",
+//     "date_of_birth":"2015-03-25T12:00:00Z",
+//     "gender":"male"
+// }
